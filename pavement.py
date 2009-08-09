d8469918baeabc784d128974f14e411dbfa04bec
Damien Lebrun <dinoboff@hotmail.com>
2009-08-09 16:33:47 +0100

    Add a list of requirement for pip

--- pavement.py
+++ pavement.py
@@ -1,24 +1,20 @@
 import os
 import sys
+import sets
 
 from paver.easy import *
 from paver.setuputils import setup
-from setuptools import find_packages
 
 try:
     from paver.virtual import bootstrap
-except:
-    info("bootstrap task not available")
-    
-try:
     from github.tools.task import (
         gh_pages_build,
         gh_pages_clean,
         gh_pages_create,
         gh_register)
     from git import Git
-except:
-    info("github-tools' task not available")
+except ImportError:
+    info("Some tasks could not been loaded")
 
 
 version='0.1.7'
@@ -38,16 +34,11 @@ classifiers = [
     ]
 
 install_requires = [
-    'setuptools',
-    'paver-templates>=0.1.0b1',
-    'GitPython',
+    'setuptools>=0.6c9',
+    'paver-templates>=0.1.0b3',
+    'GitPython>=0.1.6',
+    'simplejson',
     ]
-
-try:
-    # available with python 2.6
-    import json
-except ImportError:
-    install_requires.append('simplejson')
 
 entry_points="""
     # -*- Entry points: -*-
@@ -60,12 +51,12 @@ setup(name='github-tools',
     description='Helpers for Python package hosting at GitHub',
     long_description=long_description,
     classifiers=classifiers,
-    keywords='sphinx, github, paster',
+    keywords='sphinx github paster',
     author='Damien Lebrun',
     author_email='dinoboff@hotmail.com',
     url='http://dinoboff.github.com/github-tools/',
     license='BSD',
-    packages = find_packages('src'),
+    packages = ['github.tools'],
     package_dir = {'': 'src'},
     namespace_packages=['github'],
     include_package_data=True,
@@ -86,9 +77,9 @@ options(
         script_name='bootstrap.py',
         dest_dir='./virtual-env/',
         packages_to_install=[
-            'virtualenv',
-            'Nose',
-            'Mock',
+            'virtualenv>=1.3.3',
+            'Nose>=0.10.4',
+            'Mock>=0.5.0',
             ]
         ),
     sphinx=Bunch(
@@ -99,51 +90,66 @@ options(
     )
 
 
+    
 @task
-@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def pip_requirements():
+    """Create a pip requirement file."""
+    req = sets.Set()
+    for d in (
+        options.virtualenv.get('packages_to_install', [])
+        + options.setup.get('install_requires', [])
+        ):
+        req.add(d+'\n')
+    
+    f = open('dev-requirements.txt', 'w')
+    try:
+        f.writelines(req)
+    finally:
+        f.close()
+        
+@task
+def manifest():
+    """Generate a Manifest using 'git ls-files'"""
+    manifest_in = open('MANIFEST.in', 'w')    
+    try: 
+        includes = (
+            "include %s\n" % f 
+            for f in Git('.').ls_files().splitlines()
+            if (not os.path.basename(f).startswith('.') 
+                and f != 'docs/build/html')
+            )
+        manifest_in.writelines(includes)
+    finally:
+        manifest_in.close()
+
+@task
+@needs('pip_requirements', 'generate_setup', 'manifest', 'minilib',
+    'setuptools.command.sdist')
 def sdist():
     """Overrides sdist to make sure that our setup.py is generated."""
 
 
-if 'gh_pages_build' in globals():
-    
-    @task
-    @needs('gh_pages_build', 'github.tools.task.gh_pages_update')
-    def gh_pages_update():
-        """Overrides github.tools.task to rebuild the doc (with sphinx)."""
+@task
+@needs('gh_pages_build', 'github.tools.task.gh_pages_update')
+def gh_pages_update():
+    """Overrides github.tools.task to rebuild the doc (with sphinx)."""
     
     
-    tag_name = 'v%s' % version
+tag_name = 'v%s' % version
     
-    @task
-    def tag():
-        """tag a new version of this distribution"""
-        git = Git('.')
-        git.pull('origin', 'master')
-        git.tag(tag_name)
-    
-    @task
-    def adjust_options():
-        options.update(
-            gh_pages_update=Bunch(commit_message='Update doc to %s' % version))
-    
-    @task
-    @needs('sdist', 'tag', 'setuptools.command.upload',
-        'adjust_options', 'gh_pages_update')
-    def upload():
-        """Upload the distribution to pypi, the new tag and the doc to Github"""
-        Git('.').push('origin', 'master', tag_name)
-    
-    @task
-    def manifest():
-        """Generate a Manifest using 'git ls-files'"""
-        manifest_in = open('MANIFEST.in', 'w')    
-        try: 
-            includes = (
-                "include %s\n" % f 
-                for f in Git('.').ls_files().splitlines()
-                if not os.path.basename(f).startswith('.')
-                )
-            manifest_in.writelines(includes)
-        finally:
-            manifest_in.close()
+@task
+def tag():
+    """tag a new version of this distribution"""
+    git = Git('.')
+    git.pull('origin', 'master')
+    git.tag(tag_name)
+
+
+@task
+@needs('sdist', 'tag', 'setuptools.command.upload',)
+def upload():
+    """Upload the distribution to pypi, the new tag and the doc to Github"""
+    options.update(
+        gh_pages_update=Bunch(commit_message='Update doc to %s' % version))
+    gh_pages_update()
+    Git('.').push('origin', 'master', tag_name)
